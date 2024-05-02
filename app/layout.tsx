b3965c3fe4d40c210d8726eaf4127ebf13d2150a
Akib-Raihan-4 <raihanakib062@gmail.com>
2024-05-02 16:12:09 +0600

    dark theme using next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import RootProviders from "../components/providers/RootProviders";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Budget Tracker",
-  description: "CodeWithKliton",
+  title: "Expense Tracker",
+  description: "Store you expenses for better finance",
 };
 
 export default function RootLayout({
@@ -19,9 +20,13 @@ export default function RootLayout({
     <ClerkProvider>
       <html
         lang="en"
+        className="dark"
+        style={{
+          colorScheme: "dark",
+        }}
       >
         <body className={inter.className}>
-          {children}
+          <RootProviders>{children}</RootProviders>
         </body>
       </html>
     </ClerkProvider>
