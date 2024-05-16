231adb26a2b8235973b1e64171ddabdad0e797dc
Akib-Raihan-4 <raihanakib062@gmail.com>
2024-05-16 16:03:59 +0600

    fixed script for deployment

--- schema/userSettings.ts
+++ schema/userSettings.ts
@@ -1,7 +1,7 @@
 import { Currencies } from "@/lib/currency";
 import {z} from "zod";
 
-export const UpdateUSerCurrencySchema = z.object({
+export const UpdateUserCurrencySchema = z.object({
     currency: z.custom((value) => {
         const found = Currencies.some((c) => c.value === value);
         if(!found){
