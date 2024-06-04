23b48136b0b1df8e7fce8b70ef8ecc7922a58132
Akib-Raihan-4 <raihanakib062@gmail.com>
2024-06-04 15:27:09 +0600

    Dialog and zod schema for transactions

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,49 +1,62 @@
-import { currentUser } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation';
-import React from 'react'
-import prisma from '@/lib/prisma';
-import { Button } from '@/components/ui/button';
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
+import prisma from "@/lib/prisma";
+import { Button } from "@/components/ui/button";
+import CreateTransactionDialogue from "./_components/CreateTransactionDialogue";
 
 async function page() {
   const user = await currentUser();
 
-  if(!user){
-    redirect("/sign-in")
+  if (!user) {
+    redirect("/sign-in");
   }
 
-  const UserSettings = await prisma.userSettings.findUnique
-  ({ 
-    where: 
-    { 
-      userId: user.id, 
-    } 
-  })
+  const UserSettings = await prisma.userSettings.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
 
-  if(!UserSettings){
-    redirect("/wizard")
+  if (!UserSettings) {
+    redirect("/wizard");
   }
 
   return (
-    <div className='h-full bg-background'>
-      <div className='border-b bg-card'>
+    <div className="h-full bg-background">
+      <div className="border-b bg-card">
         <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
-          <p className='text-3xl font-bold'>Hello,{user.firstName} !</p>
+          <p className="text-3xl font-bold">Hello,{" "} {user.firstName} !</p>
           <div className="flex items-center gap-3">
-            <Button variant={'outline'}
-            className='border-emerald-500 bg-emerald-950 text-white
-            hover:bg-emerald-700'>
-              New Income
-            </Button>
-            <Button variant={'outline'}
-            className='border-emerald-500 bg-emerald-950 text-white
-            hover:bg-emerald-700'>
-              New Expense
-            </Button>
+            <CreateTransactionDialogue
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-emerald-500 bg-emerald-950 text-white
+              hover:bg-emerald-700"
+                >
+                  New Income
+                </Button>
+              }
+              type="income"
+            />
+            <CreateTransactionDialogue
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-rose-500 bg-rose-950 text-white
+            hover:bg-rose-700"
+                >
+                  New Expense
+                </Button>
+              }
+              type="expense"
+            />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
